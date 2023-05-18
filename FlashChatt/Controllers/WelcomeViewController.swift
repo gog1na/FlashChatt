4959cfcaf0b1d0bd9b2df0e69833efb5add8bb67
gog1na <129860688+gog1naa@users.noreply.github.com>
2023-05-18 17:35:53 +0400

    Animation & Firebase

--- FlashChatt/Controllers/WelcomeViewController.swift
+++ FlashChatt/Controllers/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  FlashChatt
 //
 //  Created by Giorgi Goginashvili on 5/18/23.
@@ -8,12 +8,29 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        animatedTitle()
+    }
+    
+    private func animatedTitle() {
+        titleLabel.text = ""
+        var charIndex: Double = 0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            print("-")
+            print(0.1 * charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
 
-
 }
-
